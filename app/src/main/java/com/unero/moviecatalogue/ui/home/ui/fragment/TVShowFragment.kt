d35502256f5edf19a8413b6cf6b6130fc42c54f5
Unero <ramadhanbhg@gmail.com>
2021-04-18 19:24:55 +0700

    Layout #2: - Fragment Item List

---
+++ app/src/main/java/com/unero/moviecatalogue/ui/home/ui/fragment/TVShowFragment.kt
@@ -0,0 +1,19 @@
+package com.unero.moviecatalogue.ui.home.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.unero.moviecatalogue.R
+
+class TVShowFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_t_v_show, container, false)
+    }
+}
